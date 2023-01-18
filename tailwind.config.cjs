a1d40135d148bbd3aa391d05702f963b65abc983
Angélica <angelica_shigematsu@outlook.com>
2023-01-18 20:27:09 -0300

    second class

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.tsx", 
     "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#09090A'
+      },
+      gridTemplateRows: {
+        7: 'repeat(7, minmax(0, 1fr))'
+      }
+    },
   },
   plugins: []
 }
