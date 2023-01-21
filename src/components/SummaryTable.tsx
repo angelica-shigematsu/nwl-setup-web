cd233c47febb2575fc1098267ab70074955a7f77
Angélica <angelica_shigematsu@outlook.com>
2023-01-21 09:16:33 -0300

    add popover and dialog

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -21,7 +21,7 @@ export function SummaryTable() {
       <div className="grid grid-rows-7 grid-flow-row gap-3">
       {weekDays.map((weekDay, i) => {
           return (
-            <div key={`${weekDay}-${i}`}className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center">
+            <div key={`${weekDay}-${i}`} className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center">
               {weekDay}
             </div>
           )
@@ -30,9 +30,15 @@ export function SummaryTable() {
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
       {summaryDates.map(date => {
-        return <HabitDay key={date.toString()}/>
+        return (
+        <HabitDay 
+        amount={5} 
+        completed={Math.round(Math.random()) * 5} 
+        key={date.toString()}
+        />
+        )
       })}
-
+ 
       {amountOfDaysToFill > 0 && Array.from({length: amountOfDaysToFill}).map((_, i) => {
         return (
           <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"></div>
